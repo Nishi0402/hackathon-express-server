a2be16f646ceea4fd98f9dba95e849fa1e53b9c8
Nishigandha Kolapkar <nishigandhak04@gmail.com>
2025-05-07 11:07:39 +0530

    login

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+const userRouter = require("./routes/users")
+const { jwtAuth } = require("./utils/jwtauth");
+
+// cmd> npm install cors
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json())
+//app.use(jwtAuth);
+app.use("/user", userRouter)
+
+//app.use("/orders", orderRouter)
+
+const port = 3000
+app.listen(port, "0.0.0.0", () => {
+    console.log("server ready at port", port)
+})
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,62 @@ const express = require("express")
 const bcrypt = require("bcrypt")
 const router = express.Router()
 
-//common prefix--/users
-
-//POST /user SignUp
+//common prefix--/user
 
 
+//POST /user sign in
+router.post("/signin", (req, resp) => {
+    const {email, password} = req.body
+    //console.log(req.url + " - " + req.method + " : " + email + " & " + passwd)
+    db.query("SELECT * FROM user WHERE email=?", [email],
+        (err, results) => {
+            if(err)
+                return resp.send(apiError(err))
+            //console.log("results: ", results)
+            if(results.length !== 1) // user with email not found
+                return resp.send(apiError("Invalid email"))
+            return resp.send (apiSuccess("Password match"))
+
+            const dbUser = results[0]
+            // if (!bcrypt.compareSync(password, dbUser.password))
+                //return resp.send (apiSuccess("Password match"))
+             //const isMatching = compareSync(password, dbUser.password)
+             //console.log("is passwd matching: " , isMatching)
+        //      if(!isMatching) // password not matching
+        //        return resp.send(apiError("Invalid password"))
+        //    const token = createToken(dbUser)    
+        //     resp.send(apiSuccess({...dbUser, token})) // password matched for this user
+        }
+    )
+})
+
+
+// //POST /user SignUp
+// router.post("/signup", (req, resp) => {
+//     const {firstname, lastname, email, password} = req.body
+//     //const encPasswd = bcrypt.hashSync(password, 10)
+//     db.query("INSERT INTO user (firstname, lastname, email, password) VALUES (?, ?, ?, ?)",
+//         [firstname, lastname, email, password],
+//         (err, result) => {
+//             if(err)
+//                 return resp.send(apiError(err))
+//             // if user inserted successfully, return new user object
+//             if(result.affectedRows === 1) {
+//                 db.query("SELECT * FROM user WHERE id=?", [result.insertId],
+//                     (err, results) => {
+//                         if(err)
+//                             return resp.send(apiError(err))
+//                         resp.send(apiSuccess(results[0]))
+//                     }
+//                 )
+//             }
+//         }
+//     )
+// })
+
+
+
+
+
+
+module.exports = router
